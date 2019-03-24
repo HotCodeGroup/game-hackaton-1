4f649de418c5c7f5d3f3af6171def6433e698947
Apakhov <m_apakhov@mail.ru>
2019-03-24 15:56:29 +0300

    [ADD] saveObjects logic

--- gamePingPong.ts
+++ gamePingPong.ts
@@ -7,17 +7,19 @@ class Player {
     width: number;
     vX: number;
     vY: number;
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, height: number, width: number) {
         this.x = x;
         this.y = y;
         this.vX = 0;
         this.vY = 0;
+        this.height = height;
+        this.width = width;
     }
 }
 
-function checkSpeed(p: Player){
-    let speed1 = Math.sqrt(p.vX*p.vX + p.vY*p.vY);
-    if(speed1 > 10){
+function checkSpeed(p: Player) {
+    let speed1 = Math.sqrt(p.vX * p.vX + p.vY * p.vY);
+    if (speed1 > 10) {
         p.vX = 0;
         p.vY = 0;
     }
@@ -25,7 +27,7 @@ function checkSpeed(p: Player){
 
 class PlayablePlayer extends Player {
     constructor(p: Player) {
-        super(p.x, p.y);
+        super(p.x, p.y, p.height, p.width);
     }
 
     setMoveVector(speed: number, x: number, y: number) {
@@ -44,6 +46,11 @@ class Ball {
     vX: number;
     vY: number;
     diameter: number;
+    constructor(diameter: number, vX: number, vY: number) {
+        this.diameter = diameter;
+        this.vY = vY;
+        this.vX = vX;
+    }
 }
 
 class Game {
@@ -56,6 +63,9 @@ class Game {
     constructor(fieldHeight: number, fieldWidth: number) {
         this.fieldHeight = fieldHeight;
         this.fieldWidth = fieldWidth;
+        this.player1 = new Player(fieldWidth / 10, fieldHeight / 2, fieldHeight / 5, fieldWidth / 20);
+        this.player2 = new Player(fieldWidth - fieldWidth / 10, fieldHeight / 2, fieldHeight / 5, fieldWidth / 20);
+        this.ball = new Ball(10, 5, 0);
     }
 
     getState(): any {
@@ -74,12 +84,50 @@ class Game {
         return [new PlayablePlayer(this.player2), Object.assign({}, this.player1), Object.assign({}, this.ball)];
     }
 
-    
+    ballPossitionCorrection() {
+
+    }
+
+    ballCorrectionP1() {
+        if (this.player1.x + this.player1.vX < this.ball.x + this.ball.vX) {
+            return;
+        }else{
+            
+        }
+    }
+
+    ballCorrectionP2() {
+        if (this.player1.x + this.player1.vX < this.ball.x + this.ball.vX) {
+            return;
+        }
+    }
+
+    p1PossitionCorrection() {
+
+    }
+
+    p2PossitionCorrection() {
+
+    }
 
     saveObjects(p1: Player, p2: Player) {
         checkSpeed(p1);
         checkSpeed(p2);
 
-        
+        this.player1.vX = p1.vX;
+        this.player1.vY = p1.vY;
+        this.player2.vX = p2.vX;
+        this.player2.vY = p2.vY;
+
+        this.ballCorrectionP1();
+        this.ballCorrectionP2();
+
+        this.player1.x += this.player1.vX;
+        this.player1.y += this.player1.vY;
+        this.player2.x += this.player2.vX;
+        this.player2.y += this.player2.vY;
+
+        this.p1PossitionCorrection();
+        this.p2PossitionCorrection();
     }
 }
